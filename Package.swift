4b8b394599244c96c6ff9657083d69d14bef1ce5
Nathan Herald <me@nathanherald.com>
2025-10-16 16:23:54 +0200

    Allow swift-argument-parser to be from 1.4.0 (lower)

--- Package.swift
+++ Package.swift
@@ -3,41 +3,41 @@
 import PackageDescription
 
 let package = Package(
-    name: "web-to-markdown-apple",
-    platforms: [
-        .iOS(.v16),
-        .macOS(.v14),
-        .macCatalyst(.v16),
-    ],
-    products: [
-        .library(
-            name: "WebToMarkdown",
-            targets: ["WebToMarkdown"]
-        ),
-        .executable(
-            name: "web-to-markdown",
-            targets: ["WebToMarkdownCLI"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.7.5"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
-    ],
-    targets: [
-        .target(
-            name: "WebToMarkdown",
-            dependencies: ["SwiftSoup"]
-        ),
-        .executableTarget(
-            name: "WebToMarkdownCLI",
-            dependencies: [
-                "WebToMarkdown",
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
-        ),
-        .testTarget(
-            name: "WebToMarkdownTests",
-            dependencies: ["WebToMarkdown"]
-        ),
-    ]
+  name: "web-to-markdown-apple",
+  platforms: [
+    .iOS(.v16),
+    .macOS(.v14),
+    .macCatalyst(.v16),
+  ],
+  products: [
+    .library(
+      name: "WebToMarkdown",
+      targets: ["WebToMarkdown"]
+    ),
+    .executable(
+      name: "web-to-markdown",
+      targets: ["WebToMarkdownCLI"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.7.5"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
+  ],
+  targets: [
+    .target(
+      name: "WebToMarkdown",
+      dependencies: ["SwiftSoup"]
+    ),
+    .executableTarget(
+      name: "WebToMarkdownCLI",
+      dependencies: [
+        "WebToMarkdown",
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]
+    ),
+    .testTarget(
+      name: "WebToMarkdownTests",
+      dependencies: ["WebToMarkdown"]
+    ),
+  ]
 )
